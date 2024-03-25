e534672542b751a163ab8f989ec52866196f13a0
Ravindranath Singh <ravindranathsingh391@gmail.com>
2024-03-25 18:46:16 +0530

    Done with navbar section of the page

---
+++ src/helpers/links.jsx
@@ -0,0 +1,28 @@
+const links = [
+    {
+        id: 1,
+        link: 'home'
+    },
+    {
+        id: 2,
+        link: 'about'
+    },
+    {
+        id: 3,
+        link: 'skills'
+    },
+    {
+        id: 4,
+        link: 'experience'
+    },
+    {
+        id: 1,
+        link: 'projects'
+    },
+    {
+        id: 5,
+        link: 'contact'
+    }
+]
+
+export default links;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      signature: ["Great Vibes"]
+    },
   },
   plugins: [],
 }
